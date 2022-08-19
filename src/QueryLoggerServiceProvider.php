8eefce71cb65a9d8c79628fda1ae5dac55cf11fd
amir <a.moharami@gmail.com>
2022-08-20 00:50:42 +0430

    creat first query log

--- src/QueryLoggerServiceProvider.php
+++ src/QueryLoggerServiceProvider.php
@@ -3,6 +3,7 @@
 namespace Moharami\QueryLogger;
 
 use Illuminate\Support\ServiceProvider;
+use Psy\Readline\Hoa\Event;
 
 class QueryLoggerServiceProvider extends ServiceProvider
 {
@@ -11,7 +12,8 @@ class QueryLoggerServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (env('APP_DEBUG', false)) {
+
+        if (env('APP_DEBUG', true)) {
             $this->startQueryLogger();
         }
     }
@@ -20,31 +22,31 @@ class QueryLoggerServiceProvider extends ServiceProvider
     private function startQueryLogger()
     {
         \Event::listen(
-            'illuminate.query',
-            function ( $query, $bindings, $time, $name ) {
-                $data = compact( 'bindings', 'time', 'name' );
+            'Illuminate\Database\Events\QueryExecuted',
+            function ($data) {
+//                $data = compact('bindings', 'time', 'name');
+//
+//                // Format binding data for sql insertion
+//                foreach ($bindings as $i => $binding) {
+//                    if (is_object($binding) && $binding instanceof \DateTime) {
+//                        $bindings[$i] = '\'' . $binding->format('Y-m-d H:i:s') . '\'';
+//                    } elseif (is_null($binding)) {
+//                        $bindings[$i] = 'NULL';
+//                    } elseif (is_bool($binding)) {
+//                        $bindings[$i] = $binding ? '1' : '0';
+//                    } elseif (is_string($binding)) {
+//                        $bindings[$i] = "'$binding'";
+//                    }
+//                }
+//
+//                $query = preg_replace_callback(
+//                    '/\?/',
+//                    function () use (&$bindings) {
+//                        return array_shift($bindings);
+//                    }, $query
+//                );
 
-                // Format binding data for sql insertion
-                foreach ($bindings as $i => $binding) {
-                    if (is_object( $binding ) && $binding instanceof \DateTime) {
-                        $bindings[ $i ] = '\'' . $binding->format( 'Y-m-d H:i:s' ) . '\'';
-                    } elseif (is_null( $binding )) {
-                        $bindings[ $i ] = 'NULL';
-                    } elseif (is_bool( $binding )) {
-                        $bindings[ $i ] = $binding ? '1' : '0';
-                    } elseif (is_string( $binding )) {
-                        $bindings[ $i ] = "'$binding'";
-                    }
-                }
-
-                $query = preg_replace_callback(
-                    '/\?/',
-                    function () use ( &$bindings ) {
-                        return array_shift( $bindings );
-                    }, $query
-                );
-
-                \Log::info( $query, $data );
+                \Log::info($data->sql);
             }
         );
     }
