8c690298c5160507ef9636124276fb58d1bb206f
amir <a.moharami@gmail.com>
2022-08-20 18:56:17 +0430

    add class for save pure query as a file

--- src/QueryLogger.php
+++ src/QueryLogger.php
@@ -4,5 +4,30 @@ namespace Moharami\QueryLogger;
 
 class QueryLogger
 {
-    // Build your next great package.
+
+    private string $sql;
+    private array $bindings;
+    private string $file_name = 'query.sql';
+
+    public function __construct()
+    {
+        $this->file_path = storage_path($this->file_name);
+
+        if (env('APP_DEBUG', true)) {
+            $this->startQueryLogger();
+        }
+    }
+
+    private function startQueryLogger()
+    {
+        \DB::listen(
+            function ($data) {
+                if (!empty($data->bindings)) {
+                    $data->sql = vsprintf(str_replace('?', '%s', $data->sql), $data->bindings);
+                }
+                file_put_contents($this->file_path, $data->sql . "\n", FILE_APPEND);
+            }
+        );
+    }
+
 }
--- src/QueryLoggerServiceProvider.php
+++ src/QueryLoggerServiceProvider.php
@@ -3,53 +3,16 @@
 namespace Moharami\QueryLogger;
 
 use Illuminate\Support\ServiceProvider;
-use Psy\Readline\Hoa\Event;
 
 class QueryLoggerServiceProvider extends ServiceProvider
 {
     /**
      * Bootstrap the application services.
      */
-    public function boot()
+    public function boot(QueryLogger $querylogger)
     {
-
-        if (env('APP_DEBUG', true)) {
-            $this->startQueryLogger();
-        }
     }
 
-
-    private function startQueryLogger()
-    {
-        \Event::listen(
-            'Illuminate\Database\Events\QueryExecuted',
-            function ($data) {
-//                $data = compact('bindings', 'time', 'name');
-//
-//                // Format binding data for sql insertion
-//                foreach ($bindings as $i => $binding) {
-//                    if (is_object($binding) && $binding instanceof \DateTime) {
-//                        $bindings[$i] = '\'' . $binding->format('Y-m-d H:i:s') . '\'';
-//                    } elseif (is_null($binding)) {
-//                        $bindings[$i] = 'NULL';
-//                    } elseif (is_bool($binding)) {
-//                        $bindings[$i] = $binding ? '1' : '0';
-//                    } elseif (is_string($binding)) {
-//                        $bindings[$i] = "'$binding'";
-//                    }
-//                }
-//
-//                $query = preg_replace_callback(
-//                    '/\?/',
-//                    function () use (&$bindings) {
-//                        return array_shift($bindings);
-//                    }, $query
-//                );
-
-                \Log::info($data->sql);
-            }
-        );
-    }
 
     /**
      * Register the application services.
